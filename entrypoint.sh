5a8a86c970ddf7e7920432aa9dffd09105a17b1f
suttichaip <guy.thunder.guy@gmail.com>
2021-01-06 12:11:11 +0700

    edit entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 set -e -o pipefail
- 
+
 main (){
     cmd=$1
     dir=$2
@@ -9,14 +9,14 @@ main (){
         echo "${dir}: No such file or directory exists";
         exit 1;
     fi
- 
+
     for file in $(ls ${dir}); do
         echo "Executing: deck $cmd $ops -s $dir/$file"
         deck $cmd $ops -s $dir/$file
     done
 }
- 
- 
+
+
 case $1 in
     "ping") deck $1 $3;;
     "validate"|"diff"|"sync") main $1 $2 "$3" ;;
